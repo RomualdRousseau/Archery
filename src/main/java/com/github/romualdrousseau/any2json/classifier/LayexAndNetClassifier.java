acf08ad2a99f09e0bf47cc60f3e9bf4b34c773a7
Romuald Rousseau <romualdrousseau@gmail.com>
2023-01-06 17:27:46 +0800

    Refactor and serialize model

--- src/main/java/com/github/romualdrousseau/any2json/classifier/LayexAndNetClassifier.java
+++ src/main/java/com/github/romualdrousseau/any2json/classifier/LayexAndNetClassifier.java
@@ -5,12 +5,14 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.Arrays;
-import java.util.Collections;
+import java.util.Base64;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.tensorflow.SavedModelBundle;
@@ -24,6 +26,7 @@ import com.github.romualdrousseau.any2json.ILayoutClassifier;
 import com.github.romualdrousseau.any2json.ITagClassifier;
 import com.github.romualdrousseau.any2json.layex.Layex;
 import com.github.romualdrousseau.any2json.layex.TableMatcher;
+import com.github.romualdrousseau.any2json.util.Disk;
 import com.github.romualdrousseau.shuju.json.JSON;
 import com.github.romualdrousseau.shuju.json.JSONArray;
 import com.github.romualdrousseau.shuju.json.JSONObject;
@@ -36,11 +39,17 @@ import com.github.romualdrousseau.shuju.preprocessing.tokenizer.ShingleTokenizer
 
 public class LayexAndNetClassifier implements ILayoutClassifier, ITagClassifier<List<Integer>>, AutoCloseable {
 
+    private final List<String> vocabulary;
+    private final int ngrams;
+    private final List<String> lexicon;
     private final List<String> entities;
+    private final Map<String, String> patterns;
     private final List<String> filters;
     private final List<String> tags;
     private final List<String> requiredTags;
     private final List<String> pivotEntityList;
+    private final List<String> metaLayexes;
+    private final List<String> dataLayexes;
     private final Path modelPath;
 
     private List<TableMatcher> metaMatchers;
@@ -54,26 +63,33 @@ public class LayexAndNetClassifier implements ILayoutClassifier, ITagClassifier<
     private final Text.IHasher hasher;
     private final RegexComparer comparer;
 
-    private SavedModelBundle tagClassifierModel;
-    private SessionFunction tagClassifierFunc;
+    private final SavedModelBundle tagClassifierModel;
+    private final SessionFunction tagClassifierFunc;
+    private final boolean modelIsTemp;
 
     public LayexAndNetClassifier(final List<String> vocabulary, final int ngrams, final List<String> lexicon,
             final List<String> entities, final Map<String, String> patterns, final List<String> filters,
             final List<String> tags, final List<String> requiredTags, final List<String> pivotEntityList,
             final List<String> metaLayexes, final List<String> dataLayexes, final Path modelPath) {
+        this.vocabulary = vocabulary;
+        this.ngrams = ngrams;
+        this.lexicon = lexicon;
         this.entities = entities;
+        this.patterns = patterns;
         this.filters = filters;
         this.tags = tags;
         this.requiredTags = requiredTags;
         this.pivotEntityList = pivotEntityList;
+        this.metaLayexes = metaLayexes;
+        this.dataLayexes = dataLayexes;
 
         this.metaMatchers = metaLayexes.stream().map(Layex::new).map(Layex::compile).toList();
         this.dataMatchers = dataLayexes.stream().map(Layex::new).map(Layex::compile).toList();
         this.recipe = null;
 
-        this.tokenizer = (ngrams == 0) ? new ShingleTokenizer(lexicon) : new NgramTokenizer(ngrams);
-        this.hasher = new VocabularyHasher(vocabulary);
-        this.comparer = new RegexComparer(patterns);
+        this.tokenizer = (this.ngrams == 0) ? new ShingleTokenizer(this.lexicon) : new NgramTokenizer(this.ngrams);
+        this.hasher = new VocabularyHasher(this.vocabulary);
+        this.comparer = new RegexComparer(this.patterns);
 
         this.modelPath = modelPath;
         if (modelPath.toFile().exists()) {
@@ -83,33 +99,51 @@ public class LayexAndNetClassifier implements ILayoutClassifier, ITagClassifier<
             this.tagClassifierModel = null;
             this.tagClassifierFunc = null;
         }
+        this.modelIsTemp = false;
     }
 
     public LayexAndNetClassifier(final JSONObject json) {
-        this.entities = Collections.emptyList();
-        this.filters = Collections.emptyList();
-        this.tags = Collections.emptyList();
-        this.requiredTags = Collections.emptyList();
-        this.pivotEntityList = Collections.emptyList();
+        this.vocabulary = JSON.<String>Stream(json.getJSONArray("vocabulary")).toList();
+        this.ngrams = json.getInt("ngrams");
+        this.lexicon = JSON.<String>Stream(json.getJSONArray("lexicon")).toList();
+        this.entities = JSON.<String>Stream(json.getJSONArray("entities")).toList();
+        this.patterns = JSON.<JSONObject>Stream(json.getJSONArray("patterns")).collect(Collectors.toMap(x -> x.getString("pattern"), x -> x.getString("type")));
+        this.filters = JSON.<String>Stream(json.getJSONArray("filters")).toList();
+        this.tags = JSON.<String>Stream(json.getJSONArray("tags")).toList();
+        this.requiredTags = JSON.<String>Stream(json.getJSONArray("requiredTags")).toList();
+        this.pivotEntityList = JSON.<String>Stream(json.getJSONArray("pivotEntityList")).toList();
+        this.metaLayexes = JSON.<String>Stream(json.getJSONArray("metaLayexes")).toList();
+        this.dataLayexes = JSON.<String>Stream(json.getJSONArray("dataLayexes")).toList();
 
-        this.metaMatchers = Collections.emptyList();
-        this.dataMatchers = Collections.emptyList();
+        this.metaMatchers = metaLayexes.stream().map(Layex::new).map(Layex::compile).toList();
+        this.dataMatchers = dataLayexes.stream().map(Layex::new).map(Layex::compile).toList();
         this.recipe = null;
 
-        this.tokenizer = new ShingleTokenizer(Collections.emptyList());
-        this.hasher = new VocabularyHasher(Collections.emptyList());
-        this.comparer = new RegexComparer(Collections.emptyMap());
+        this.tokenizer = (this.ngrams == 0) ? new ShingleTokenizer(this.lexicon) : new NgramTokenizer(this.ngrams);
+        this.hasher = new VocabularyHasher(this.vocabulary);
+        this.comparer = new RegexComparer(this.patterns);
 
-        this.modelPath = null;
-        this.tagClassifierModel = null;
-        this.tagClassifierFunc = null;
+        this.modelPath = this.JSONStringToModel(json.getString("model"));
+        if (modelPath.toFile().exists()) {
+            this.tagClassifierModel = SavedModelBundle.load(modelPath.toString(), "serve");
+            this.tagClassifierFunc = this.tagClassifierModel.function(Signature.DEFAULT_KEY);
+        } else {
+            this.tagClassifierModel = null;
+            this.tagClassifierFunc = null;
+        }
+        this.modelIsTemp = true;
     }
 
     @Override
     public void close() {
+        if (this.modelIsTemp) {
+            try {
+                Disk.deleteDir(this.modelPath);
+            } catch (IOException ignore) {
+            }
+        }
         if (this.tagClassifierModel != null) {
             this.tagClassifierModel.close();
-            this.tagClassifierModel = null;
         }
     }
 
@@ -209,7 +243,7 @@ public class LayexAndNetClassifier implements ILayoutClassifier, ITagClassifier<
             put("context_input", ListIntegertoTFloat32(predictSet, 15, 115));
         }};
         final Map<String, org.tensorflow.Tensor> result = this.tagClassifierFunc.call(inputs);
-        return this.tags.get((int) TFloat32ToTensor((TFloat32) result.get("tag_output")).argmax(0).item(0)); 
+        return this.tags.get((int) TFloat32ToShujuTensor((TFloat32) result.get("tag_output")).argmax(0).item(0)); 
     }
 
     @Override
@@ -248,22 +282,6 @@ public class LayexAndNetClassifier implements ILayoutClassifier, ITagClassifier<
         }
     }
 
-    private TFloat32 ListIntegertoTFloat32(List<Integer> l, int a, int b) {
-        float[][] result = new float[1][b - a];
-        for(int i = a, j = 0; i < b; i++, j++) {
-            result[0][j] = (float) l.get(i);
-        }
-        return TFloat32.tensorOf(StdArrays.ndCopyOf(result));
-    }
-
-    private Tensor TFloat32ToTensor(TFloat32 t) {
-        float[] result = new float[(int) t.shape().size(1)];
-        for(int i = 0; i < result.length; i++) {
-            result[i] = t.getFloat(0, i);
-        }
-        return Tensor.create(result);
-    }
-
     @Override
     public float getAccuracy() {
         return this.accuracy;
@@ -276,8 +294,42 @@ public class LayexAndNetClassifier implements ILayoutClassifier, ITagClassifier<
 
     @Override
     public JSONObject toJSON() {
-        JSONObject result = JSON.newJSONObject();
+        final JSONObject result = JSON.newJSONObject();
+        result.setJSONArray("vocabulary", JSON.<String>parseJSONArray(this.vocabulary));
+        result.setInt("ngram", this.ngrams);
+        result.setJSONArray("lexicon", JSON.<String>parseJSONArray(this.lexicon));
+        result.setJSONArray("entities", JSON.<String>parseJSONArray(this.entities));
+        result.setJSONObject("patterns", JSON.<String>parseJSONObject(this.patterns));
+        result.setJSONArray("filters", JSON.<String>parseJSONArray(this.filters));
+        result.setJSONArray("tags", JSON.<String>parseJSONArray(this.tags));
+        result.setJSONArray("requiredTags", JSON.<String>parseJSONArray(this.requiredTags));
+        result.setJSONArray("pivotEntityList", JSON.<String>parseJSONArray(this.pivotEntityList));
+        result.setJSONArray("metaLayexes", JSON.<String>parseJSONArray(this.metaLayexes));
+        result.setJSONArray("dataLayexes", JSON.<String>parseJSONArray(this.dataLayexes));
+        result.setString("model", this.modelToJSONString(this.modelPath));
         return result;
+    }
+
+    private String modelToJSONString(final Path modelPath) {
+        try {
+            final Path temp = Files.createTempFile("model-", ".zip");
+            Disk.zipDir(modelPath, temp.toFile());
+            return Base64.getEncoder().encodeToString(Files.readAllBytes(temp));
+        } catch (final IOException x) {
+            throw new RuntimeException(x);
+        }
+    }
+
+    private Path JSONStringToModel(final String modelString) {
+        try {
+            final Path temp1 = Files.createTempFile("model-", ".zip");
+            final Path modelPath = Files.createTempDirectory("model-");
+            Files.write(temp1, Base64.getDecoder().decode(modelString), StandardOpenOption.CREATE);
+            Disk.unzipDir(temp1, modelPath);
+            return modelPath;
+        } catch (final IOException x) {
+            throw new RuntimeException(x);
+        }
     }
 
     private void installKernels(final Path destPath) {
@@ -285,24 +337,35 @@ public class LayexAndNetClassifier implements ILayoutClassifier, ITagClassifier<
             if(destPath.toFile().exists()) {
                 return;
             }
-            Path sourcePath = Paths.get(LayexAndNetClassifier.class.getResource("/kernels").toURI());
+            final Path sourcePath = Paths.get(LayexAndNetClassifier.class.getResource("/kernels").toURI());
             if (!sourcePath.toFile().isDirectory()) {
                 return;
             }
             Arrays.asList(sourcePath.toFile().listFiles()).stream().forEach(k -> {
-                Arrays.asList(k.listFiles()).stream().forEach(f -> {
-                    final Path src = f.toPath();
-                    final Path dst = destPath.resolve(sourcePath.relativize(f.toPath()));
-                    try {
-                        dst.getParent().toFile().mkdirs();
-                        Files.copy(src, dst);
-                    } catch (IOException x) {
-                        throw new RuntimeException(x);
-                    }
-                });
+                try {
+                    Disk.copyDir(k.toPath(), destPath.resolve(sourcePath.relativize(k.toPath())));
+                } catch (IOException x) {
+                    throw new RuntimeException(x);
+                }
             });
-        } catch (URISyntaxException x) {
+        } catch (final URISyntaxException x) {
             throw new RuntimeException(x);
         }
+    }
+
+    private TFloat32 ListIntegertoTFloat32(final List<Integer> l, final int a, final int b) {
+        final float[][] result = new float[1][b - a];
+        for(int i = a, j = 0; i < b; i++, j++) {
+            result[0][j] = (float) l.get(i);
+        }
+        return TFloat32.tensorOf(StdArrays.ndCopyOf(result));
+    }
+
+    private Tensor TFloat32ToShujuTensor(final TFloat32 t) {
+        final float[] result = new float[(int) t.shape().size(1)];
+        for(int i = 0; i < result.length; i++) {
+            result[i] = t.getFloat(0, i);
+        }
+        return Tensor.create(result);
     }
 }
