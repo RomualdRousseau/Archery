7dcce297380cfa2cb4031a715fb86c7e3fb5134c
Romuald Rousseau <romuald.rousseau@servier.com>
2023-08-03 15:33:00 +0000

    Cleanup

--- src/main/java/com/github/romualdrousseau/any2json/loader/csv/CsvClass.java
+++ src/main/java/com/github/romualdrousseau/any2json/loader/csv/CsvClass.java
@@ -1,10 +1,6 @@
 package com.github.romualdrousseau.any2json.loader.csv;
 
-import java.io.File;
-import java.util.Optional;
-
 import com.github.romualdrousseau.any2json.Document;
-import com.github.romualdrousseau.any2json.DocumentFactory;
 import com.github.romualdrousseau.any2json.IDocumentClass;
 
 public class CsvClass implements IDocumentClass
