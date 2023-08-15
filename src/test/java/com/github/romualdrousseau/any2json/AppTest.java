a6ab20001970b1c39504e1439b07626712aea376
Romuald Rousseau <romuald.rousseau@servier.com>
2023-08-15 16:18:12 +0800

    Add test

---
+++ src/test/java/com/github/romualdrousseau/any2json/AppTest.java
@@ -0,0 +1,16 @@
+package com.github.romualdrousseau.any2json;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void test() {
+    }
+}
