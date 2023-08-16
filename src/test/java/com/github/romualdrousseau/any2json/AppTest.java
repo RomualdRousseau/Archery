8368150560d101c3893966c25024b0308df931e7
Romuald Rousseau <romuald.rousseau@servier.com>
2023-08-16 18:28:55 +0800

    Fix test

--- src/test/java/com/github/romualdrousseau/any2json/AppTest.java
+++ src/test/java/com/github/romualdrousseau/any2json/AppTest.java
@@ -2,6 +2,7 @@ package com.github.romualdrousseau.any2json;
 
 import java.nio.file.Path;
 import java.util.EnumSet;
+import java.util.stream.StreamSupport;
 import java.net.URL;
 import java.io.File;
 import java.net.URISyntaxException;
@@ -17,7 +18,7 @@ public class AppTest {
      * Rigorous Test :-)
      */
     @Test
-    public void testReadVariousDocuments() {
+    public void testReadWithIntelliLayexAndTag() {
         final File file = this.getResourceFile("HongKong - ZUELLIG - Sales - 20220305.xlsx");
 
         final Model model = ModelDB.createConnection("sales-english");
@@ -25,7 +26,33 @@ public class AppTest {
         try (final Document doc = DocumentFactory.createInstance(file, "UTF-8")
                 .setModel(model)
                 .setHints(EnumSet.of(Document.Hint.INTELLI_LAYOUT, Document.Hint.INTELLI_TAG))) {
-            doc.sheets().forEach(s -> s.getTable().ifPresent(t -> t.headers().forEach(h -> System.out.println(h.getName()))));
+            doc.sheets().forEach(s -> s.getTable().ifPresent(t -> t.headers().forEach(h -> System.out.print(h.getTag().getValue() + " "))));
+            System.out.println();
+            doc.sheets().forEach(s -> s.getTable().ifPresent(t -> StreamSupport.stream(t.rows().spliterator(), false).limit(5).forEach(r -> {
+                r.cells().forEach(c -> System.out.print(c.getValue() + " "));
+                System.out.println();
+            })));
+        }
+    }
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void testReadWithIntelliLayex() {
+        final File file = this.getResourceFile("HongKong - ZUELLIG - Sales - 20220305.xlsx");
+
+        final Model model = ModelDB.createConnection("sales-english");
+
+        try (final Document doc = DocumentFactory.createInstance(file, "UTF-8")
+                .setModel(model)
+                .setHints(EnumSet.of(Document.Hint.INTELLI_LAYOUT))) {
+            doc.sheets().forEach(s -> s.getTable().ifPresent(t -> t.headers().forEach(h -> System.out.print(h.getTag().getValue() + " "))));
+            System.out.println();
+            doc.sheets().forEach(s -> s.getTable().ifPresent(t -> StreamSupport.stream(t.rows().spliterator(), false).limit(5).forEach(r -> {
+                r.cells().forEach(c -> System.out.print(c.getValue() + " "));
+                System.out.println();
+            })));
         }
     }
 
