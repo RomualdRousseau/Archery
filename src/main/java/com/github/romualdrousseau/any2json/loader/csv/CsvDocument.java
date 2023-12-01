351ddd77452871b4266074b70461677737a79b20
Romuald Rousseau <romuald.rousseau@servier.com>
2023-12-01 18:41:03 +0800

    Lazy loading

--- src/main/java/com/github/romualdrousseau/any2json/loader/csv/CsvDocument.java
+++ src/main/java/com/github/romualdrousseau/any2json/loader/csv/CsvDocument.java
@@ -5,7 +5,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 
 import com.github.romualdrousseau.any2json.Document;
 import com.github.romualdrousseau.any2json.Sheet;
@@ -14,27 +13,29 @@ import com.github.romualdrousseau.any2json.base.BaseSheet;
 import com.github.romualdrousseau.any2json.parser.sheet.SimpleSheetParser;
 import com.github.romualdrousseau.any2json.transform.op.DropColumnsWhenFillRatioLessThan;
 import com.github.romualdrousseau.any2json.util.Disk;
-import com.github.romualdrousseau.shuju.bigdata.DataFrame;
-import com.github.romualdrousseau.shuju.bigdata.DataFrameWriter;
-import com.github.romualdrousseau.shuju.bigdata.Row;
 import com.github.romualdrousseau.shuju.strings.StringUtils;
-import com.github.romualdrousseau.shuju.types.Tensor;
 
 
 public class CsvDocument extends BaseDocument {
 
-    private static final int BATCH_SIZE = 10000;
-    private static final String[] SEPARATORS = { "\t", ",", ";" };
+    private CsvSheet sheet;
 
     @Override
     public boolean open(final File txtFile, final String encoding, final String password) {
+        if (txtFile == null) {
+            throw new IllegalArgumentException();
+        }
+
         this.sheet = null;
 
-        if (encoding != null && this.openWithEncoding(txtFile, encoding)) {
-            return true;
-        } else if (encoding != null) {
-            return this.openWithEncoding(txtFile, "UTF-8");
-        } else {
+        try {
+            if (encoding != null) {
+                return this.openWithEncoding(txtFile, encoding);
+            } else {
+                return this.openWithEncoding(txtFile, "UTF-8");
+            }
+        } catch (final IOException x) {
+            this.close();
             return false;
         }
     }
@@ -46,12 +47,10 @@ public class CsvDocument extends BaseDocument {
                 this.sheet.close();
                 this.sheet = null;
             }
-            if (this.rows != null) {
-                this.rows.close();
-                this.rows = null;
-            }
         } catch (final IOException x) {
             // throw new UncheckedIOException(x);
+        } finally {
+            super.close();
         }
     }
 
@@ -61,8 +60,13 @@ public class CsvDocument extends BaseDocument {
     }
 
     @Override
+    public String getSheetNameAt(final int i) {
+        return this.sheet.getName();
+    }
+
+    @Override
     public Sheet getSheetAt(final int i) {
-        return new BaseSheet(this, this.sheet.getName(), this.sheet);
+        return new BaseSheet(this, this.sheet.getName(), this.sheet.ensureDataLoaded());
     }
 
     @Override
@@ -79,39 +83,14 @@ public class CsvDocument extends BaseDocument {
         this.setSheetParser(new SimpleSheetParser());
     }
 
-    private boolean openWithEncoding(final File txtFile, final String encoding) {
-        if (txtFile == null || encoding == null) {
-            throw new IllegalArgumentException();
+    private boolean openWithEncoding(final File txtFile, final String encoding) throws IOException{
+        final var reader = new BufferedReader(new InputStreamReader(new FileInputStream(txtFile), encoding));
+        if (encoding.equals("UTF-8")) {
+            this.processBOM(reader);
         }
-
-        try (
-            final var reader = new BufferedReader(new InputStreamReader(new FileInputStream(txtFile), encoding));
-            final var writer = new DataFrameWriter(BATCH_SIZE);
-            ) {
-
-            if (encoding.equals("UTF-8")) {
-                this.processBOM(reader);
-            }
-
-            this.rows = this.processRows(reader, writer);
-            if (this.rows.getRowCount() > 0) {
-                final String sheetName = Disk.removeExtension(txtFile.getName());
-                this.sheet = new CsvSheet(sheetName, this.rows);
-            }
-
-            return this.sheet != null;
-
-        } catch (final IOException x) {
-            return false;
-        }
-    }
-
-    private boolean checkIfGoodEncoding(final String[] row) {
-        boolean result = true;
-        for (int i = 0; i < row.length; i++) {
-            result &= StringUtils.checkIfGoodEncoding(row[i]);
-        }
-        return result;
+        final var sheetName = Disk.removeExtension(txtFile.getName());
+        this.sheet = new CsvSheet(sheetName, reader);
+        return true;
     }
 
     private void processBOM(final BufferedReader reader) throws IOException {
@@ -121,97 +100,4 @@ public class CsvDocument extends BaseDocument {
             reader.reset();
         }
     }
-
-    private DataFrame processRows(final BufferedReader reader, final DataFrameWriter writer) throws IOException {
-        var firstPass = true;
-        for (String textRow; (textRow = reader.readLine()) != null;) {
-
-            if (firstPass) {
-                this.separator = this.guessSeparator(textRow);
-            }
-
-            final String[] cells = parseOneRow(textRow);
-
-            if (firstPass) {
-                if (!this.checkIfGoodEncoding(cells)) {
-                    throw new IOException("CSV bad encoding");
-                }
-                firstPass = false;
-            }
-
-            for (int j = 0; j < cells.length; j++) {
-                cells[j] = StringUtils.cleanToken(cells[j]);
-            }
-
-            writer.write(Row.of(cells));
-        }
-
-        return writer.getDataFrame();
-    }
-
-    private String[] parseOneRow(final String data) {
-        final var result = new ArrayList<String>();
-        var acc = "";
-        var state = 0;
-
-        final char[] tmp = data.toCharArray();
-        for (int i = 0; i < tmp.length; i++) {
-            final char c = tmp[i];
-
-            switch (state) {
-                case 0:
-                    if (c == separator.charAt(0)) {
-                        result.add(acc);
-                        acc = "";
-                    } else if (c == '"' && acc.trim().equals("")) {
-                        acc += c;
-                        state = 1;
-                    } else {
-                        acc += c;
-                    }
-                    break;
-
-                case 1: // Double quote context
-                    if (c == '"') {
-                        acc += c;
-                        state = 2;
-                    } else {
-                        acc += c;
-                    }
-                    break;
-
-                case 2: // Check double quote context exit
-                    if (c == '"') {
-                        state = 1;
-                    } else if (c == separator.charAt(0)) {
-                        result.add(acc);
-                        acc = "";
-                        state = 0;
-                    } else {
-                        acc += c;
-                        state = 0;
-                    }
-                    break;
-            }
-        }
-
-        if (!acc.trim().equals("")) {
-            result.add(acc);
-        }
-
-        return result.toArray(String[]::new);
-    }
-
-    private String guessSeparator(final String sample) {
-        // find the separator generating the more of columns
-        final float[] v = new float[SEPARATORS.length];
-        for (int i = 0; i < SEPARATORS.length; i++) {
-            v[i] = sample.split(SEPARATORS[i], -1).length;
-        }
-        return SEPARATORS[(int) Tensor.of(v).argmax(0).item(0)];
-    }
-
-    private CsvSheet sheet;
-    private String separator;
-    private DataFrame rows;
 }
--- src/main/java/com/github/romualdrousseau/any2json/loader/csv/CsvSheet.java
+++ src/main/java/com/github/romualdrousseau/any2json/loader/csv/CsvSheet.java
@@ -1,26 +1,40 @@
 package com.github.romualdrousseau.any2json.loader.csv;
 
+import java.io.BufferedReader;
 import java.io.Closeable;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import com.github.romualdrousseau.any2json.base.PatcheableSheetStore;
 import com.github.romualdrousseau.shuju.bigdata.DataFrame;
+import com.github.romualdrousseau.shuju.bigdata.DataFrameWriter;
 import com.github.romualdrousseau.shuju.bigdata.Row;
 import com.github.romualdrousseau.shuju.strings.StringUtils;
+import com.github.romualdrousseau.shuju.types.Tensor;
 
 class CsvSheet extends PatcheableSheetStore implements Closeable {
 
-    private final String name;
-    private final DataFrame rows;
+    private static final int BATCH_SIZE = 10000;
+    private static final String[] SEPARATORS = { "\t", ",", ";" };
 
-    public CsvSheet(final String name, final DataFrame rows) {
+    private final String name;
+    private BufferedReader reader;
+    private DataFrame rows;
+
+    public CsvSheet(final String name, final BufferedReader reader) {
         this.name = name;
-        this.rows = rows;
+        this.reader = reader;
+        this.rows = null;
     }
 
     @Override
     public void close() throws IOException {
-        this.rows.close();
+        if (this.rows != null) {
+            this.rows.close();
+        }
+        if (this.reader != null) {
+            this.reader.close();
+        }
     }
 
     public String getName() {
@@ -53,13 +67,125 @@ class CsvSheet extends PatcheableSheetStore implements Closeable {
         if (patchCell != null) {
             return patchCell;
         } else {
-            return StringUtils.cleanToken(this.getCellAt(colIndex, rowIndex));
+            return this.getCellAt(colIndex, rowIndex);
         }
     }
 
     @Override
     public int getNumberOfMergedCellsAt(final int colIndex, final int rowIndex) {
         return 1;
+    }
+
+    public CsvSheet ensureDataLoaded() {
+        if (this.rows != null) {
+            return this;
+        }
+        try (final var writer = new DataFrameWriter(BATCH_SIZE);) {
+            this.rows = this.processRows(this.reader, writer);
+            this.reader.close();
+            this.reader = null;
+            return this;
+        } catch (final IOException x) {
+            return this;
+        }
+    }
+
+    private DataFrame processRows(final BufferedReader reader, final DataFrameWriter writer) throws IOException {
+        var firstPass = true;
+        var separator = ",";
+        for (String textRow; (textRow = reader.readLine()) != null;) {
+
+            if (firstPass) {
+                separator = this.guessSeparator(textRow);
+            }
+
+            final String[] cells = parseOneRow(textRow, separator);
+
+            if (firstPass) {
+                if (!this.checkIfGoodEncoding(cells)) {
+                    throw new IOException("CSV bad encoding");
+                }
+                firstPass = false;
+            }
+
+            for (int j = 0; j < cells.length; j++) {
+                cells[j] = StringUtils.cleanToken(cells[j]);
+            }
+
+            writer.write(Row.of(cells));
+        }
+
+        return writer.getDataFrame();
+    }
+
+    private String[] parseOneRow(final String data, final String separator) {
+        final var result = new ArrayList<String>();
+        var acc = "";
+        var state = 0;
+
+        final char[] tmp = data.toCharArray();
+        for (int i = 0; i < tmp.length; i++) {
+            final char c = tmp[i];
+
+            switch (state) {
+                case 0:
+                    if (c == separator.charAt(0)) {
+                        result.add(acc);
+                        acc = "";
+                    } else if (c == '"' && acc.trim().equals("")) {
+                        acc += c;
+                        state = 1;
+                    } else {
+                        acc += c;
+                    }
+                    break;
+
+                case 1: // Double quote context
+                    if (c == '"') {
+                        acc += c;
+                        state = 2;
+                    } else {
+                        acc += c;
+                    }
+                    break;
+
+                case 2: // Check double quote context exit
+                    if (c == '"') {
+                        state = 1;
+                    } else if (c == separator.charAt(0)) {
+                        result.add(acc);
+                        acc = "";
+                        state = 0;
+                    } else {
+                        acc += c;
+                        state = 0;
+                    }
+                    break;
+            }
+        }
+
+        if (!acc.trim().equals("")) {
+            result.add(acc);
+        }
+
+        return result.toArray(String[]::new);
+    }
+
+    private boolean checkIfGoodEncoding(final String[] row) {
+        boolean result = true;
+        for (int i = 0; i < row.length; i++) {
+            result &= StringUtils.checkIfGoodEncoding(row[i]);
+        }
+        return result;
+    }
+
+    private String guessSeparator(final String sample) {
+        // find the separator generating the more of columns
+        final float[] v = new float[SEPARATORS.length];
+        for (int i = 0; i < SEPARATORS.length; i++) {
+            v[i] = sample.split(SEPARATORS[i], -1).length;
+        }
+        return SEPARATORS[(int) Tensor.of(v).argmax(0).item(0)];
     }
 
     private String getCellAt(final int colIndex, final int rowIndex) {
