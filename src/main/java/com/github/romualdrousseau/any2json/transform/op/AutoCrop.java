5e3ee367e3945b0ca740c022ec1bec94d401d4a8
Romuald Rousseau <romualdrousseau@gmail.com>
2024-08-07 11:55:56 +0800

    Implement autocrop (#24)
    Co-authored-by: Romuald Rousseau <romuald.rousseau@servier.com>

--- src/main/java/com/github/romualdrousseau/any2json/transform/op/AutoCrop.java
+++ src/main/java/com/github/romualdrousseau/any2json/transform/op/AutoCrop.java
@@ -1,10 +1,85 @@
 package com.github.romualdrousseau.any2json.transform.op;
 
 import com.github.romualdrousseau.any2json.base.BaseSheet;
+import com.github.romualdrousseau.shuju.strings.StringUtils;
 
 public class AutoCrop {
 
     public static void Apply(final BaseSheet sheet) {
-        // TODO: to be implemented
+        AutoCrop._cropLeft(sheet);
+        AutoCrop._cropRight(sheet);
+        sheet.removeAllNullColumns();
+
+        AutoCrop._cropTop(sheet);
+        AutoCrop._cropBottom(sheet);
+        sheet.removeAllNullRows();
+    }
+
+    public static void _cropLeft(final BaseSheet sheet) {
+        for(int j = 0; j <= sheet.getLastColumnNum(); j++) {
+            int emptyCount = sheet.getLastRowNum() + 1;
+            for(int i = 0; i <= sheet.getLastRowNum(); i++) {
+                if(sheet.hasCellDataAt(j, i) && !StringUtils.isFastBlank(sheet.getCellDataAt(j, i))) {
+                    emptyCount--;
+                }
+            }
+            final float m = 1.0f - (float) emptyCount / (float) (sheet.getLastRowNum() + 1);
+            if (m <= 0) {
+                sheet.markColumnAsNull(j);
+            } else {
+                return;
+            }
+        }
+    }
+
+    public static void _cropRight(final BaseSheet sheet) {
+        for(int j = sheet.getLastColumnNum(); j >= 0 ; j--) {
+            int emptyCount = sheet.getLastRowNum() + 1;
+            for(int i = 0; i <= sheet.getLastRowNum(); i++) {
+                if(sheet.hasCellDataAt(j, i) && !StringUtils.isFastBlank(sheet.getCellDataAt(j, i))) {
+                    emptyCount--;
+                }
+            }
+            final float m = 1.0f - (float) emptyCount / (float) (sheet.getLastRowNum() + 1);
+            if (m <= 0) {
+                sheet.markColumnAsNull(j);
+            } else {
+                return;
+            }
+        }
+    }
+
+    public static void _cropTop(final BaseSheet sheet) {
+        for(int i = 0; i <= sheet.getLastRowNum(); i++) {
+            int emptyCount = sheet.getLastColumnNum() + 1;
+            for(int j = 0; j <= sheet.getLastColumnNum(i); j++) {
+                if(sheet.hasCellDataAt(j, i) && !StringUtils.isFastBlank(sheet.getCellDataAt(j, i))) {
+                    emptyCount--;
+                }
+            }
+            final float m = 1.0f - (float) emptyCount / (float) (sheet.getLastColumnNum() + 1);
+            if (m <= 0) {
+                sheet.markRowAsNull(i);
+            } else {
+                return;
+            }
+        }
+    }
+
+    public static void _cropBottom(final BaseSheet sheet) {
+        for(int i = sheet.getLastRowNum(); i >= 0; i--) {
+            int emptyCount = sheet.getLastColumnNum() + 1;
+            for(int j = 0; j <= sheet.getLastColumnNum(i); j++) {
+                if(sheet.hasCellDataAt(j, i) && !StringUtils.isFastBlank(sheet.getCellDataAt(j, i))) {
+                    emptyCount--;
+                }
+            }
+            final float m = 1.0f - (float) emptyCount / (float) (sheet.getLastColumnNum() + 1);
+            if (m <= 0) {
+                sheet.markRowAsNull(i);
+            } else {
+                return;
+            }
+        }
     }
 }
