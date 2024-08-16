4f8dbc70cc53bbba9c66ab9e0d34bd59350b3adc
Romuald Rousseau <romuald.rousseau@servier.com>
2024-08-16 16:41:42 +0800

    refacto: Rewrite tests

--- src/test/java/com/github/romualdrousseau/any2json/ModelDB.java
+++ src/test/java/com/github/romualdrousseau/any2json/ModelDB.java
@@ -1,23 +1,16 @@
 package com.github.romualdrousseau.any2json;
 
+import java.io.IOException;
 import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.file.Path;
-
-import com.github.romualdrousseau.shuju.json.JSON;
 
 public class ModelDB {
 
     public static Model createConnection(final String modelName) {
-        return new Model(JSON.loadObject(ModelDB.getResourcePath(String.format("/data/%s.json", modelName))));
-    }
-
-    private static Path getResourcePath(final String resourceName) {
         try {
-            final URL resourceUrl = new ModelDB().getClass().getResource(resourceName);
-            assert resourceUrl != null : resourceName + " not found";
-            return Path.of(resourceUrl.toURI());
-        } catch (final URISyntaxException x) {
+            return new ModelBuilder()
+                    .fromResource(new ModelDB().getClass(), String.format("/data/%s.json", modelName))
+                    .build();
+        } catch (final URISyntaxException | IOException x) {
             assert false : x.getMessage();
             return null;
         }
