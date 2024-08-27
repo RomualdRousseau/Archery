ae00795fa9d52c8fb7c2dea20a1ccdb06302958d
Romuald Rousseau <romuald.rousseau@servier.com>
2024-08-27 14:08:13 +0800

    chore: Bring back code

--- any2json-llm-classifier/src/main/java/com/github/romualdrousseau/any2json/classifier/LLMTagClassifier.java
+++ any2json-llm-classifier/src/main/java/com/github/romualdrousseau/any2json/classifier/LLMTagClassifier.java
@@ -1,23 +1,35 @@
 package com.github.romualdrousseau.any2json.classifier;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.StreamSupport;
 
+import com.github.romualdrousseau.shuju.json.JSON;
+import com.github.romualdrousseau.shuju.json.JSONObject;
+import com.github.romualdrousseau.any2json.Header;
 import com.github.romualdrousseau.any2json.Model;
+import com.github.romualdrousseau.any2json.Table;
 import com.github.romualdrousseau.any2json.TagClassifier;
+import com.theokanning.openai.completion.chat.ChatCompletionRequest;
+import com.theokanning.openai.completion.chat.ChatMessage;
+import com.theokanning.openai.service.OpenAiService;
 
 public class LLMTagClassifier extends SimpleTagClassifier {
 
-    public LLMTagClassifier(final List<String> vocabulary, final int ngrams, final int wordMinSize,
-            final List<String> lexicon, final Optional<Path> modelPath) {
-    }
+    private final static String OPENAI_KEY = System.getenv("OPENAI_KEY");
 
     public LLMTagClassifier(final Model model, final TagClassifier.TagStyle tagStyle) {
+        super(model, tagStyle);
+        this.service = new OpenAiService(OPENAI_KEY);
+        this.tags = null;
     }
 
     @Override
     public void close() throws Exception {
+        this.tags = null;
     }
 
     @Override
@@ -25,7 +37,52 @@ public class LLMTagClassifier extends SimpleTagClassifier {
     }
 
     @Override
-    public String predict(final String name, final List<String> entities, final List<String> context) {
-        return "none";
+    public String predict(final Table table, final Header header) {
+        if (this.tags != null) {
+            return this.tags.<String>get(header.getName()).orElse("none");
+        }
+
+        final var csvContent = new StringBuilder();
+        table.headers().forEach(h -> csvContent.append(h.getName() + ","));
+        csvContent.replace(csvContent.length() - 1, csvContent.length(), "\n");
+        StreamSupport.stream(table.rows().spliterator(), false).limit(10).forEach(r -> {
+            if (r.getNumberOfCells() > 0) {
+                r.cells().forEach(c -> csvContent.append("\"" + c.getValue() + "\","));
+                csvContent.replace(csvContent.length() - 1, csvContent.length(), "\n");
+            }
+        });
+
+        final var definitions = String.join("\n", this.getModel().getData().getList("definitions"));
+
+        final var content = String.join("\n", this.getResourceContent("/prompt-template.txt"));
+        final var message = new ChatMessage("user", content
+                .replace("{DEF}", definitions)
+                .replace("{CSV}", csvContent.toString()));
+
+        final var completionRequest = ChatCompletionRequest.builder()
+                .messages(List.of(message))
+                .model("gpt-3.5-turbo")
+                .temperature(0.0)
+                .build();
+        this.service.createChatCompletion(completionRequest).getChoices().forEach(x -> {
+            this.tags = JSON.objectOf(x.getMessage().getContent());
+        });
+
+        System.out.println(this.tags.toString(true));
+
+        return this.tags.<String>get(header.getName()).orElse("none");
     }
+
+    private List<String> getResourceContent(String resourceName) {
+        try {
+            final var resourceUrl = this.getClass().getResource(resourceName);
+            assert resourceUrl != null : resourceName + " not found";
+            return Files.readAllLines(Path.of(resourceUrl.toURI()));
+        } catch (URISyntaxException | IOException x) {
+            throw new RuntimeException(x);
+        }
+    }
+
+    private OpenAiService service;
+    private JSONObject tags;
 }
---
+++ any2json-llm-classifier/src/test/java/com/github/romualdrousseau/any2json/classifier/Test_LLM.java
@@ -0,0 +1,84 @@
+package com.github.romualdrousseau.any2json.classifier;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Path;
+import java.util.EnumSet;
+import java.util.stream.StreamSupport;
+
+import org.junit.Test;
+
+import com.github.romualdrousseau.any2json.Document;
+import com.github.romualdrousseau.any2json.DocumentFactory;
+import com.github.romualdrousseau.any2json.modeldata.DataContractModelBuilder;
+
+/**
+ * Unit test for the TrainingEntry class.
+ */
+public class Test_LLM {
+
+    @Test
+    public void testDocumentPanama() throws IOException, URISyntaxException {
+        final var model = new DataContractModelBuilder()
+                .fromResource(getClass(), "/data-contract-lowes.yml")
+                .setLexicon("english")
+                .build();
+
+        final var file = this.getResourcePath("/data/Panama - BH - LOWES - Sales - 20201201.xlsx");
+        try (final var doc = DocumentFactory.createInstance(file.toFile(), "UTF-8")
+                .setHints(EnumSet.of(Document.Hint.INTELLI_TAG))
+                .setModel(model)) {
+            doc.getSheetAt(0).getTable().ifPresent(t -> {
+                final var csvContent = new StringBuilder();
+                t.headers().forEach(h -> csvContent.append(h.getName() + " (" + h.getTag().getValue() + "),"));
+                csvContent.replace(csvContent.length() - 1, csvContent.length(), "\n");
+                StreamSupport.stream(t.rows().spliterator(), false).forEach(r -> {
+                    if (r.getNumberOfCells() > 0) {
+                        r.cells().forEach(c -> csvContent.append("\"" + c.getValue() + "\","));
+                        csvContent.replace(csvContent.length() - 1, csvContent.length(), "\n");
+                    }
+                });
+                System.out.println(csvContent.toString());
+            });
+        }
+
+    }
+
+    @Test
+    public void testDocumentHongKong() throws IOException, URISyntaxException {
+        final var model = new DataContractModelBuilder()
+                .fromResource(getClass(), "/data-contract-zuellig.yml")
+                .setLexicon("english")
+                .build();
+
+        final var file = this.getResourcePath("/data/Malaysia - ZUELLIG - Sales - 20201002.xlsx");
+        try (final var doc = DocumentFactory.createInstance(file.toFile(), "UTF-8")
+                .setHints(EnumSet.of(Document.Hint.INTELLI_TAG))
+                .setModel(model)) {
+            doc.getSheetAt(0).getTable().ifPresent(t -> {
+                final var csvContent = new StringBuilder();
+                t.headers().forEach(h -> csvContent.append(h.getName() + " (" + h.getTag().getValue() + "),"));
+                csvContent.replace(csvContent.length() - 1, csvContent.length(), "\n");
+                StreamSupport.stream(t.rows().spliterator(), false).forEach(r -> {
+                    if (r.getNumberOfCells() > 0) {
+                        r.cells().forEach(c -> csvContent.append("\"" + c.getValue() + "\","));
+                        csvContent.replace(csvContent.length() - 1, csvContent.length(), "\n");
+                    }
+                });
+                System.out.println(csvContent.toString());
+            });
+        }
+
+    }
+
+    private Path getResourcePath(String resourceName) {
+        try {
+            URL resourceUrl = this.getClass().getResource(resourceName);
+            assert resourceUrl != null : resourceName + " not found";
+            return Path.of(resourceUrl.toURI());
+        } catch (URISyntaxException x) {
+            throw new RuntimeException(x);
+        }
+    }
+}
