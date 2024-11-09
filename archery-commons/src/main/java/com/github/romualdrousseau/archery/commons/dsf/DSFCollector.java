17b8e8cd0b4a4b2b0dd743ce551c02f34afa94d9
Romuald Rousseau <romualdrousseau@gmail.com>
2024-11-10 02:01:53 +0800

    fix: Fix pattern order to ensure reproductability (#29)
    Co-authored-by: Romuald Rousseau <romuald.rousseau@servier.com>

--- archery-commons/src/main/java/com/github/romualdrousseau/archery/commons/dsf/DSFCollector.java
+++ archery-commons/src/main/java/com/github/romualdrousseau/archery/commons/dsf/DSFCollector.java
@@ -1,5 +1,6 @@
 package com.github.romualdrousseau.archery.commons.dsf;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
@@ -12,5 +13,11 @@ public class DSFCollector {
 
     public static <T> Collector<DSFObject, ?, Map<String, T>> toUnmodifiableMap(final String key, final String value) {
         return Collectors.toUnmodifiableMap(x -> x.<String>get(key).get(), x -> x.<T>get(value).get());
+    }
+
+    public static <T> Collector<DSFObject, ?, Map<String, T>> toLinkedMap(final String key, final String value) {
+        return Collectors.toMap(x -> x.<String>get(key).get(), x -> x.<T>get(value).get(), (e1, e2) -> {
+            throw new RuntimeException();
+        }, LinkedHashMap::new);
     }
 }
