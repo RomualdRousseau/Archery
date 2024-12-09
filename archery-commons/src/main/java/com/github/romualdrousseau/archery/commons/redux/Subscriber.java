9c4e6712275039aa510977d9850527a0e045e29e
Romuald Rousseau <romualdrousseau@gmail.com>
2024-12-09 23:06:08 +0800

    fix: Fix the data loss (#67)
    Co-authored-by: Romuald Rousseau <romuald.rousseau@servier.com>

--- archery-commons/src/main/java/com/github/romualdrousseau/archery/commons/redux/Subscriber.java
+++
@@ -1,6 +0,0 @@
-package com.github.romualdrousseau.archery.commons.redux;
-
-import java.util.function.BiConsumer;
-
-@FunctionalInterface
-public interface Subscriber<S, A extends Action> extends BiConsumer<Store<S, A>, A> {}
--- archery-commons/src/test/java/com/github/romualdrousseau/archery/commons/python/Test_Python.java
+++ archery-commons/src/test/java/com/github/romualdrousseau/archery/commons/python/Test_Python.java
@@ -6,6 +6,7 @@ import java.text.ParseException;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.Date;
+import java.util.Locale;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Tag;
@@ -15,8 +16,18 @@ public class Test_Python {
     @Test
     @Tag("unit")
     public void testPythonSimpleDateformat() throws ParseException {
-        final PythonSimpleDateFormat formatter = new PythonSimpleDateFormat("%a,%d/%m/%y");
+        final var formatter = new PythonSimpleDateFormat("%a,%d/%m/%y");
         assertEquals("Sun,24/09/23", formatter.format(Date.from(LocalDate.of(2023, 9, 24).atStartOfDay(ZoneId.systemDefault()).toInstant())));
         assertEquals("Sun,05/12/99", formatter.format(formatter.parse("Sun,05/12/99")));
+    }
+
+    @Test
+    @Tag("unit")
+    public void testPythonSimpleDateformatWithLocale() throws ParseException {
+        final var formatterGB = new PythonSimpleDateFormat("%b, %Y", Locale.forLanguageTag("en-GB"));
+        assertEquals("Sept, 2023", formatterGB.format(formatterGB.parse("Sept, 2023")));
+
+        final var formatterUS = new PythonSimpleDateFormat("%b, %Y", Locale.forLanguageTag("en-US"));
+        assertEquals("Sep, 2023", formatterUS.format(formatterUS.parse("Sep, 2023")));
     }
 }
