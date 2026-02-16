615f55a7c75f4e5290827010e32d200ec66dc118
Romuald Rousseau <romualdrousseau@gmail.com>
2026-02-16 12:42:34 +0100

    feat: Add auto split control and allow disabling this feature (#120)
    * feat: Add auto split control and allow disabling this feature
    
    * chore: Remove LLM classifier due to openai-service obsolescence
    
    ---------
    
    Co-authored-by: Romuald Rousseau <romuald.rousseau@servier.com>

--- archery/src/main/java/com/github/romualdrousseau/archery/intelli/IntelliTableStrategyWithPivot.java
+++ archery/src/main/java/com/github/romualdrousseau/archery/intelli/IntelliTableStrategyWithPivot.java
@@ -33,6 +33,9 @@ public class IntelliTableStrategyWithPivot extends IntelliTableStrategy {
     private Optional<Row> emitOneRowWithPivot(final List<BaseHeader> headers, final BaseTableGraph graph,
             final DataTable orgTable,
             final BaseRow orgRow, final RowGroup rowGroup, final PivotEntry pivotEntry, final String typeValue) {
+        if (orgRow.isEmpty()) {
+            return Optional.empty();
+        }
         if (!this.isValidPivotEntry(orgRow, pivotEntry, typeValue)) {
             return Optional.empty();
         }
